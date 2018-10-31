e4e18b07a75a1c9386d76c38c6fbb22bc9f55481
Anton Lukin <anton@lukin.me>
2018-11-01 01:48:05 +0300

    add setup single line command

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Custom dotfiles for Ubuntu and macOS.
 
+## Installation
+
+`git clone https://github.com/antonlukin/dotfiles.git dotfiles-$(date +%F) && dotfiles-$(date +%F)/setup.sh && rm -rf dotfiles-$(date +%F)`
--- setup.sh
+++ setup.sh
@@ -3,23 +3,23 @@
 # get cufrent dir
 dotdir=`dirname $0`
 if [[ $dotdir = . ]]; then
-	dotdir=`pwd`
+    dotdir=`pwd`
 fi
 
 
 # extract vim configs
 if [[ -f $HOME/.vimrc ]]; then
-	cp $HOME/.vimrc $HOME/.vimrc.backup
+    cp $HOME/.vimrc $HOME/.vimrc.backup
 fi
 
 rm -rf ~/.vim
 cp -R $dotdir/vim/.vim ~/.vim
-cp -R $dotdir/vim/.vimrc ~/.vimrc 
+cp -R $dotdir/vim/.vimrc ~/.vimrc
 
 
 # extract zsh configs
 if [[ -f ~/.zshrc ]]; then
-	cp ~/.zshrc ~/.zshrc.backup
+    cp ~/.zshrc ~/.zshrc.backup
 fi
 
 cp -R $dotdir/zsh/.zshrc ~/.zshrc
@@ -27,16 +27,16 @@ cp -R $dotdir/zsh/.zshrc ~/.zshrc
 
 # extract git configs
 if [[ -f ~/.gitignore ]]; then
-	cp ~/.gitignore ~/.gitignore.backup
-fi  
+    cp ~/.gitignore ~/.gitignore.backup
+fi
 
 cp $dotdir/git/.gitignore ~/.gitignore
 
 if [[ -f ~/.gitconfig ]]; then
-	cp ~/.gitconfig ~/.gitconfig.backup
-fi   
+    cp ~/.gitconfig ~/.gitconfig.backup
+fi
 
-cp $dotdir/git/.gitconfig ~/.gitconfig 
+cp $dotdir/git/.gitconfig ~/.gitconfig
 
 
 echo "dotfiles installed"
